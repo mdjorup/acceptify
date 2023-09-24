07b688e4d18b7e6f4d5d9180acda805f75e03646
mdjorup <michael.djorup@gmail.com>
2023-09-24 17:25:01 -0400

    run prettier

--- types/supabase.ts
+++ types/supabase.ts
@@ -4,44 +4,44 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json | undefined }
-  | Json[]
+  | Json[];
 
 export interface Database {
   public: {
     Tables: {
       users: {
         Row: {
-          createdAt: string
-          subscriptionId: number
-          userId: string
-        }
+          createdAt: string;
+          subscriptionId: number;
+          userId: string;
+        };
         Insert: {
-          createdAt?: string
-          subscriptionId?: number
-          userId: string
-        }
+          createdAt?: string;
+          subscriptionId?: number;
+          userId: string;
+        };
         Update: {
-          createdAt?: string
-          subscriptionId?: number
-          userId?: string
-        }
-        Relationships: []
-      }
-    }
+          createdAt?: string;
+          subscriptionId?: number;
+          userId?: string;
+        };
+        Relationships: [];
+      };
+    };
     Views: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Functions: {
       requesting_user_id: {
-        Args: Record<PropertyKey, never>
-        Returns: string
-      }
-    }
+        Args: Record<PropertyKey, never>;
+        Returns: string;
+      };
+    };
     Enums: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     CompositeTypes: {
-      [_ in never]: never
-    }
-  }
+      [_ in never]: never;
+    };
+  };
 }
