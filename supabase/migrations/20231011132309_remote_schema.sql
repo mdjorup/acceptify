597989f0904b89271d28737c5d1138a103c5a29a
mdjorup <michael.djorup@gmail.com>
2023-10-11 09:32:01 -0400

    db migrations

---
+++ supabase/migrations/20231011132309_remote_schema.sql
@@ -0,0 +1,7 @@
+alter table "public"."profiles" alter column "id" drop default;
+
+alter table "public"."profiles" add constraint "profiles_id_fkey" FOREIGN KEY (id) REFERENCES auth.users(id) ON UPDATE CASCADE ON DELETE CASCADE not valid;
+
+alter table "public"."profiles" validate constraint "profiles_id_fkey";
+
+
